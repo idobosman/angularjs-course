9ac7597e499298b26ec8f7bb5f0f7bb854456511
Ido Bosman <ido_bosman@hotmail.com>
2014-10-13 15:25:01 +0200

    Lab 1, Exercise 2: Hello World

--- app01/app/scripts/controllers/main.js
+++ app01/app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('app01App')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.content = "Hello world!";
   });
